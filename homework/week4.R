345b311b23b3767a595a59c6204edd92fd41c931
Jonathan Marshall <jmarshall@never.you.mind>
2024-08-05 13:50:57 +1200

    adds homework for week 4

---
+++ homework/week4.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+# read in data: you need an internet connection for this
+odis <- read_csv("https://www.massey.ac.nz/~jcmarsha/data/cricket/ODIs.csv")
+
+# print out the dataset
+odis
+
+# use the space below to answer the questions in the quiz
